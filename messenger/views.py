d8a38de4a85a0600aedcb0dafc22e8470d5b4703
Prabhanshu Abhishek <pabhi@iitk.ac.in>
2017-04-08 11:53:28 +0530

    Login page gives error, and redirect to home if no error.

--- messenger/views.py
+++ messenger/views.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Users
 from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('messenger/index.html')
+    template = loader.get_template('messenger/login.html')
     context = {'empty':'',}
     return HttpResponse(template.render(context, request))
 
@@ -17,12 +19,16 @@ def results(request,response):
                 return HttpResponse(str(response))
 
 def login(request):
-	p_username = request.POST['username']
-	p_password = request.POST['password']
-	ans = 0
-	users_list = Users.objects.all()
-	for u in users_list:
-		if u.username==p_username and u.password==p_password:
-			ans = 1
-	return HttpResponseRedirect(reverse('messenger:results', args=(ans,)))
+    p_username = request.POST['username']
+    p_password = request.POST['password']
+	# ans = 0
+    try:
+        user = Users.objects.get(username=p_username)
+        if user.password == p_password:
+            return render(request,'messenger/home.html',{'user':user})
+        else:
+            error="Incorrect Password for username : "+p_username
+    except ObjectDoesNotExist:
+        error="No user with username '"+p_username+"' exists."
+    return render(request,'messenger/login.html',{'error':error})
 
