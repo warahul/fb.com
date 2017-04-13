9945eda5cf0b1760f172a07da53ffdaea8d24219
Shaswat Chaubey <schaubey@iitk.ac.in>
2017-04-13 17:44:29 +0530

    Seen backend done

--- messenger/models.py
+++ messenger/models.py
@@ -13,3 +13,10 @@ class Messeges(models.Model):
         users= models.ManyToManyField(User,related_name="msgList")
         def __str__(self):              # __unicode__ on Python 2
                 return self.messege
+
+class Seen(models.Model):
+        users=models.ManyToManyField(User,related_name="Unneccessary")
+        count=models.IntegerField(default=0)
+        curr_user=models.ForeignKey(User,related_name="UserSeenList")
+        def __str__(self):              # __unicode__ on Python 2
+                return self.count
